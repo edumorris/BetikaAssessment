948f918b7946bdead0f69e17e869c17a94c990f4
edumorris <eduuwachira@gmail.com>
2023-04-11 18:43:36 +0300

    Login fix, register url check add.

--- src/test/java/com/betika/steps/LoginSteps.java
+++ src/test/java/com/betika/steps/LoginSteps.java
@@ -16,15 +16,16 @@ import java.time.Duration;
 
 import static com.betika.utility.BrowserUtil.waitFor;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
 
 import static com.betika.utility.ConfigReader.confRead;
 import static com.betika.utility.Driver.getDriver;
+import static org.hamcrest.Matchers.*;
 
 public class LoginSteps {
     LoginPage loginPage = new LoginPage();
     HomePageCommons homeCommons = new HomePageCommons();
     String loginUrl = confRead("betika_url") + confRead("login_subdirectory");
+    String registerUrl = confRead("betika_url") + "register";
 
     WebDriver driver = getDriver();
     @Given("user is on the login page")
@@ -47,7 +48,7 @@ public class LoginSteps {
     public void theUserShouldNotBeLoggedIn() {
         waitFor(5);
 
-        assertThat(driver.getCurrentUrl(), is(loginUrl));
+        assertThat(driver.getCurrentUrl(), anyOf(is(loginUrl), is(registerUrl)));
     }
 
     @And("the {string} prompt should appear")
